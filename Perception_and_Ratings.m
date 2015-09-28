5817a9554cdcb92784b722bb218167845fe153e8
leakp <lea.kampermann@gmail.com>
2015-09-28 16:53:41 +0200

    relating perception and fear tunings, started only

--- Perception_and_Ratings.m
+++ Perception_and_Ratings.m
@@ -18,8 +18,38 @@ for i=1:length(g1.tunings{4}.singlesubject)
 end
 AMPL_mean = mean(AMPL,1);
 
-%%1500ms
+alpha_incr=[];
+for i=1:length(g1.tunings{4}.singlesubject)
+alpha_incr = [alpha_incr; ((g1.pmf.params1(1,1,i)-g1.pmf.params1(3,1,i))-(g1.pmf.params1(2,1,i)-g1.pmf.params1(4,1,i)))];
+end
+
+
+%%
+%600ms
 p               = Project;
-g2 = Group(Project.subjects_600); 
+mask            = p.getMask('RATE');
+subjects        = intersect(find(mask),Project.subjects_600);
+g2              = Group(subjects);
 n2 = length(g2.ids);
+
+g2.getSI(3);
+FWHM=[];
+for i=1:length(g2.tunings{4}.singlesubject)
+    FWHM(i,:)= [g2.tunings{3}.singlesubject{i}.Est(2) g2.tunings{4}.singlesubject{i}.Est(2)];
+end
+FWHM_mean = mean(FWHM,1);
+AMPL=[];
+for i=1:length(g2.tunings{4}.singlesubject)
+    AMPL(i,:)= [g2.tunings{3}.singlesubject{i}.Est(1) g2.tunings{4}.singlesubject{i}.Est(1)];
+end
+AMPL_mean = mean(AMPL,1);
+
+alpha_incr=[];
+for i=1:length(g2.tunings{4}.singlesubject)
+alpha_incr = [alpha_incr; ((g2.pmf.params1(1,1,i)-g2.pmf.params1(3,1,i))-(g2.pmf.params1(2,1,i)-g2.pmf.params1(4,1,i)))];
+end
+%%
+g = 
+
+
 
