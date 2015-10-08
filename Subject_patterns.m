883a7a6c8a3dc1eea5d89e0a2e73524df74861f6
leakp <lea.kampermann@gmail.com>
2015-10-08 19:22:49 +0200

    no big changes

--- Subject_patterns.m
+++ Subject_patterns.m
@@ -136,3 +136,78 @@ end
 fix.getmaps(v{:});
 close all
 
+%% correlations within vs between subjects
+clear all
+p               = Project;
+mask            = p.getMask('ET_Discr');
+subjects        = intersect(find(mask),Project.subjects_1500);
+mask            = p.getMask('ET_feargen');
+subjects        = intersect(find(mask),subjects);
+mask            = p.getMask('PMF');
+subjects        = intersect(find(sum(mask,2)==4),subjects);
+mask            = p.getMask('RATE');
+subjects        = intersect(find(mask),subjects);
+g               = Group(subjects);
+
+phase = [1 2 3 4 5];
+fix   = Fixmat(subjects,phase);
+v=[];
+c=0;
+for sub = subjects'
+    for ph = phase
+    c=c+1;
+    v{c} = {'subject' sub 'phase' ph};
+    end
+end
+fix.getmaps(v{:});
+%cocktail blank
+%fix.maps = fix.maps - repmat(mean(fix.maps,3),[1 1 size(fix.maps,3)]);
+fix.maps = imresize(fix.maps,[50 50]); 
+subj_maps = fix.vectorize_maps;
+imagesc(corr(subj_maps));
+axis off
+axis image
+print -dbitmap;
+
+corrmat = corr(subj_maps);
+%extract a subject's consistence (across phase correlation)
+subcorr=[];
+start = -4;
+cc = 0;
+for i = 1:length(subjects)
+    start = start+5;
+    cc = cc+1;
+    subcor(cc) = mean(squareform(corrmat(start:(start+4),start:(start+4))-1)+1);
+    start:(start+4);
+end
+%% within phases, between subjects correlation
+v=[];
+c=0;
+for ph = phase
+    for sub = subjects'
+        c=c+1;
+        v{c} = {'subject' sub 'phase' ph};
+    end
+end
+fix.getmaps(v{:});
+%cocktail blank
+%fix.maps = fix.maps - repmat(mean(fix.maps,3),[1 1 size(fix.maps,3)]);
+fix.maps = imresize(fix.maps,[50 50]); 
+subj_maps = fix.vectorize_maps;
+corrmat = corr(subj_maps);
+imagesc(corrmat);
+axis off
+axis image
+print -dbitmap;
+
+phasecor=[];
+start = -length(subjects)+1;
+cc=0;
+for i = 1:length(phases)
+    start = start+length(subjects);
+    cc = cc+1;
+    phasecor(cc) = mean(squareform(corrmat(start:start+(length(subjects)-1),start:start+(length(subjects)-1))-1)+1);
+    start,start+(length(subjects)-1)
+end
+
+
