c9f6b1968433f9c1c2a063e6606fcc47846720f9
leakp <lea.kampermann@gmail.com>
2016-02-08 20:54:59 +0100

    work in progress, kappa

--- Perception_and_Ratings.m
+++ Perception_and_Ratings.m
@@ -1,3 +1,7 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% alpha
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 p    = Project;
 mask = p.getMask('PMF');
 Subjects = intersect(find(sum(mask,2)==4),[Project.subjects_1500]);
@@ -9,16 +13,23 @@ g6 = Group(Subjects);
 mat6 = g6.parameterMat;
 clear g6
 mat = [mat15;mat6];
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% alpha
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+load('C:\Users\user\Documents\Experiments\FearCloud_Eyelab\data\midlevel\pmf_data_N51.mat','mat','tags','mat15','mat6')
 %% plot all alphas for N=51, 1500 and 600 seperately
-bar([1 2 4 5 7 8 10 11 13 14 16 17],[mean(mat(:,1)) mean(mat(:,2)) mean(mat(:,3)) mean(mat(:,4)) mean(mat15(:,1)) mean(mat15(:,2)) mean(mat15(:,3)) mean(mat15(:,4)) mean(mat6(:,1)) mean(mat6(:,2)) mean(mat6(:,3)) mean(mat6(:,4))],'FaceColor','white')
+bar([1 2 4 5 8 9 11 12 15 16 18 19],[mean(mat(:,1)) mean(mat(:,2)) mean(mat(:,3)) mean(mat(:,4)) mean(mat15(:,1)) mean(mat15(:,2)) mean(mat15(:,3)) mean(mat15(:,4)) mean(mat6(:,1)) mean(mat6(:,2)) mean(mat6(:,3)) mean(mat6(:,4))],'FaceColor','white')
 hold on;
-errorbar([1 2 4 5 7 8 10 11 13 14 16 17],[mean(mat(:,1)) mean(mat(:,2)) mean(mat(:,3)) mean(mat(:,4)) mean(mat15(:,1)) mean(mat15(:,2)) mean(mat15(:,3)) mean(mat15(:,4)) mean(mat6(:,1)) mean(mat6(:,2)) mean(mat6(:,3)) mean(mat6(:,4))],[std(mat(:,1))./sqrt(51) std(mat(:,2))./sqrt(51) std(mat(:,3))./sqrt(51) std(mat(:,4))./sqrt(51) std(mat15(:,1))./sqrt(25) std(mat15(:,2))./sqrt(25) std(mat15(:,3))./sqrt(25) std(mat15(:,4))./sqrt(25) std(mat6(:,1))./sqrt(26) std(mat6(:,2))./sqrt(26) std(mat6(:,3))./sqrt(26) std(mat6(:,4))./sqrt(26)],'k.')
+errorbar([1 2 4 5 8 9 11 12 15 16 18 19],[mean(mat(:,1)) mean(mat(:,2)) mean(mat(:,3)) mean(mat(:,4)) mean(mat15(:,1)) mean(mat15(:,2)) mean(mat15(:,3)) mean(mat15(:,4)) mean(mat6(:,1)) mean(mat6(:,2)) mean(mat6(:,3)) mean(mat6(:,4))],[std(mat(:,1))./sqrt(51) std(mat(:,2))./sqrt(51) std(mat(:,3))./sqrt(51) std(mat(:,4))./sqrt(51) std(mat15(:,1))./sqrt(25) std(mat15(:,2))./sqrt(25) std(mat15(:,3))./sqrt(25) std(mat15(:,4))./sqrt(25) std(mat6(:,1))./sqrt(26) std(mat6(:,2))./sqrt(26) std(mat6(:,3))./sqrt(26) std(mat6(:,4))./sqrt(26)],'k.')
 set(gca,'xtick',[1.5 4.5 8.5 11.5 15.5 18.5],'xticklabel',{'CS+' 'CS-' 'CS+' 'CS-' 'CS+' 'CS-'})
 
-%% plot actual figure
+%% plot differences for CSP and CSN
+clf
+bar([1 2 4 5 7 8],[mean(mat(:,1)-mat(:,2)) mean(mat(:,3)-mat(:,4)) mean(mat15(:,1)-mat15(:,2)) mean(mat15(:,3)-mat15(:,4)) mean(mat6(:,1)-mat6(:,2)) mean(mat6(:,3)-mat6(:,4))],'FaceColor','white')
+hold on;
+errorbar([1 2 4 5 7 8],[mean(mat(:,1)-mat(:,2)) mean(mat(:,3)-mat(:,4)) mean(mat15(:,1)-mat15(:,2)) mean(mat15(:,3)-mat15(:,4)) mean(mat6(:,1)-mat6(:,2)) mean(mat6(:,3)-mat6(:,4))],...
+   [std(mat(:,1)-mat(:,2))./sqrt(length(mat)) std(mat(:,3)-mat(:,4))./sqrt(length(mat)) std(mat15(:,1)-mat15(:,2))./sqrt(length(mat15)) std(mat15(:,3)-mat15(:,4))./sqrt(length(mat15)) std(mat6(:,1)-mat6(:,2))./sqrt(length(mat6)) std(mat6(:,3)-mat6(:,4))./sqrt(length(mat6))],'k.')
+set(gca,'xtick',[1.5 4.5 7.5],'xticklabel',{'pooled' '1500ms' '600ms'})
+
+%% plot resnik figure
 %better worse
 csp_better = length(find(mat(:,1)-mat(:,3)>0))./length(mat(:,1));
 csn_better = length(find(mat(:,2)-mat(:,4)>0))./length(mat(:,2));
@@ -32,36 +43,65 @@ x2 = [ones(n1,1); repmat(2,N1-n1,1); ones(n2,1); repmat(2,N2-n2,1)];
 [tbl,chi2stat,pval] = crosstab(x1,x2)
 
 %%
-load('C:\Users\onat\Desktop\Lea\behavioral_data.mat','data')
-[h,p,ci,stats] = ttest(data(:,1),data(:,3))%before the experiment, CSP vs CSN
+[h,p,ci,stats] = ttest(mat(:,1),mat(:,2))%csp before to after
+[h,p,ci,stats] = ttest(mat(:,3),mat(:,4))%csn before to after
+[h,p,ci,stats] = ttest(mat(:,1)-mat(:,2),mat(:,3)-mat(:,4))%csp improvement different than csn improvement?
 
-[h,p,ci,stats] = ttest(data(:,1),data(:,2))%CSP before to after
-[h,p,ci,stats] = ttest(data(:,3),data(:,4))%CSN before to after
+[h,p,ci,stats] = ttest(mat15(:,1),mat15(:,2))%csp before to after
+[h,p,ci,stats] = ttest(mat15(:,3),mat15(:,4))%csn before to after
+[h,p,ci,stats] = ttest(mat15(:,1)-mat15(:,2),mat15(:,3)-mat15(:,4))%csp improvement diff than csn improvement
 
-[h,p,ci,stats] = ttest(data(:,9),data(:,10),'tail','right') % csp_imprv vs csn_imprv
+[h,p,ci,stats] = ttest(mat6(:,1),mat6(:,2))%csp before to after
+[h,p,ci,stats] = ttest(mat6(:,3),mat6(:,4))%csn before to after
+[h,p,ci,stats] = ttest(mat6(:,1)-mat6(:,2),mat6(:,3)-mat6(:,4))%csp improvement diff than csn improvement
 %% 
+clf
+[r,pval]=corr(mat15);
+hold off
+imagesc(r)
+set(gca,'XTick',1:length(mat15),'YTick',1:length(mat15),'YTickLabel',tags)
+axis image
+colorbar
+hold on;
+[y x]=ind2sub(size(r),find(pval<0.05));
+text(x,y,'x','FontSize',8)
+[y x]=ind2sub(size(r),find(pval<0.01));
+text(x,y,'x','FontSize',11)
+[y x]=ind2sub(size(r),find(pval<0.001));
+text(x,y,'x','FontSize',15)
+
+
+%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % RATE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 p    = Project;
-mask = p.getMask('RATE');
-Subjects = intersect(find(mask),[Project.subjects_1500 Project.subjects_600]);
-g = Group(Subjects);
-g.getSI(3);
-[mat tags] = g.parameterMat;
+mask = p.getMask('RATEgauss');
+Subjects = intersect(find(mask),Project.subjects_1500);
+g15 = Group(Subjects);
+g15.getSI(3);
+[mat15 tags] = g15.parameterMat;
+clear g15
+Subjects = intersect(find(mask),Project.subjects_600);
+g6 = Group(Subjects);
+g6.getSI(3);
+[mat6 tags] = g6.parameterMat;
+clear g6
+mat = [mat15;mat6];
+
+
 
 figure
-bar([1 2 3 6 7 8 10 11 12],[mean(mat(:,12)) mean(mat(:,13)) mean(mat(:,14)) mean(mat(1:18,12)) mean(mat(1:18,13)) mean(mat(1:18,14)) mean(mat(19:end,12)) mean(mat(19:end,13)) mean(mat(19:end,14))],'facecolor','white')
+bar([1 2 3 6 7 8 11 12 13],[mean(mat(:,12)) mean(mat(:,13)) mean(mat(:,14)) mean(mat15(:,12)) mean(mat15(:,13)) mean(mat15(:,14)) mean(mat6(:,12)) mean(mat6(:,13)) mean(mat6(:,14))],'facecolor','white')
 hold on;
-errorbar([1 2 3 6 7 8 10 11 12],[mean(mat(:,12)) mean(mat(:,13)) mean(mat(:,14)) mean(mat(1:18,12)) mean(mat(1:18,13)) mean(mat(1:18,14)) mean(mat(19:end,12)) mean(mat(19:end,13)) mean(mat(19:end,14))],...
-    [std(mat(:,12))./sqrt(37) std(mat(:,13))./sqrt(37) std(mat(:,14))./sqrt(37) std(mat(1:18,12))./sqrt(18) std(mat(1:18,13))./sqrt(18) std(mat(1:18,14))./sqrt(18) std(mat(19:end,12))./sqrt(19) std(mat(19:end,13))./sqrt(19) std(mat(19:end,14))./sqrt(19)],'k.')
-set(gca,'xticklabel',{'Cond_all' 'Test_all' 'SI_all' 'Cond_1500' 'Test_1500' 'SI_1500' 'Cond_600' 'Test_600' 'SI_600'})
+errorbar([1 2 3 6 7 8 11 12 13],[mean(mat(:,12)) mean(mat(:,13)) mean(mat(:,14)) mean(mat15(:,12)) mean(mat15(:,13)) mean(mat15(:,14)) mean(mat6(:,12)) mean(mat6(:,13)) mean(mat6(:,14))],...
+    [std(mat(:,12))./sqrt(length(mat)) std(mat(:,13))./sqrt(length(mat)) std(mat(:,14))./sqrt(length(mat)) std(mat15(:,12))./sqrt(length(mat15)) std(mat15(:,13))./sqrt(length(mat15)) std(mat15(:,14))./sqrt(length(mat15))...
+    mean(mat6(:,12))./sqrt(length(mat6)) mean(mat6(:,13))./sqrt(length(mat6)) mean(mat6(:,14))./sqrt(length(mat6))],'k.')
+set(gca,'xticklabel',{'Cond' 'Test' 'SI' 'Cond' 'Test' 'SI' 'Cond' 'Test' 'SI'})
 
-figure;
-bar([1 2 3],mean(mat(:,12:14)));hold on;
-errorbar([1 2 3],mean(mat(:,12:14)),std(mat(:,12:14))./sqrt(length(mat)),'k.','LineWidth',2)
-
-[h,p,ci,stats] = ttest(data(:,12),data(:,13))
+[h,p,ci,stats] = ttest(mat(:,12),mat(:,13))
+[h,p,ci,stats] = ttest(mat15(:,12),mat15(:,13))
+[h,p,ci,stats] = ttest(mat6(:,12),mat6(:,13))
 
 % plot bars and Gaussians in Feargen Colors
 %
@@ -104,8 +144,8 @@ subjects        = intersect(find(sum(mask,2)==4),Project.subjects_1500);
 mask            = p.getMask('RATE');
 subjects        = intersect(find(mask),subjects);
 g15               = Group(subjects);
-g15.getSI(3)
-mat15 = g15.parameterMat;
+g15.getSI(8)
+[mat15, tags] = g15.parameterMat;
 clear g15;
 p               = Project;
 mask            = p.getMask('PMF');
@@ -113,14 +153,21 @@ subjects        = intersect(find(sum(mask,2)==4),Project.subjects_600);
 mask            = p.getMask('RATE');
 subjects        = intersect(find(mask),subjects);
 g6               = Group(subjects);
-g6.getSI(3)
+g6.getSI(8)
 mat6 = g6.parameterMat;
 clear g6;
 mat = [mat15;mat6];
+
+mat(:,end+1) = mean(mat(:,[1 3]),2);
+mat15(:,end+1) = mean(mat15(:,[1 3]),2);
+mat6(:,end+1) = mean(mat6(:,[1 3]),2);
+tags{end+1} = 'initial_alpha'
+%%
+load('C:\Users\user\Documents\Experiments\FearCloud_Eyelab\data\midlevel\rate_and_pmf_N48.mat','mat','tags','mat15','mat6')
 %% corr alpha x feargen
-[r,p]=corrcoef(mean(mat(:,[1 3]),2),mat(:,12))
-[r,p]=corrcoef(mean(mat(:,[1 3]),2),mat(:,13))
-[r,p]=corrcoef(mean(mat(:,[1 3]),2),mat(:,14))
+[r,pval]=corrcoef(mean(mat(:,[1 3]),2),mat(:,12))
+[r,pval]=corrcoef(mean(mat(:,[1 3]),2),mat(:,13))
+[r,pval]=corrcoef(mean(mat(:,[1 3]),2),mat(:,14))
 
 
 % plot correlations between threshold and sigma/SI
@@ -139,23 +186,16 @@ xlabel(sprintf('sigma_{test} (in deg)'));
 axis square
 set(gca,'fontsize',20)
 
-
 subplot(1,3,3)
 plot(mat(:,14),mean(mat(:,[1 3]),2),'k.','Markersize',25);box off;lsline
 title(sprintf('r = %1.2g',corr2(mean(mat(:,[1 3]),2),mat(:,14))))
-xlabel(sprintf(sprintf('sigma_{cond} - sigma_{test} (in deg) \n Sharpening Index')))
+xlabel(sprintf(sprintf('Sharpening Index')))
 axis square
 set(gca,'fontsize',20)
 
-%% all correlations
-clf
-mat(:,15) = mean(mat(:,[1 3]),2);
-mat(:,16) = mean(mat(:,[2 4]),2);
-tags{15} = 'alpha_before';
-tags{16} = 'alpha_after';
 %% 
 clf
-[r,pval]=corr(mat);
+[r,pval]=corr(matbig);
 hold off
 imagesc(r)
 set(gca,'XTick',1:length(mat),'YTick',1:length(mat),'YTickLabel',tags)
@@ -168,4 +208,7 @@ text(x,y,'x','FontSize',8)
 text(x,y,'x','FontSize',11)
 [y x]=ind2sub(size(r),find(pval<0.001));
 text(x,y,'x','FontSize',15)
+VerticalXlabel(tags,'interpreter','none')
+
+
 
