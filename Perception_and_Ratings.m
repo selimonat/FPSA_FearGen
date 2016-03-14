f1c3d242afd38ddbc3b77d60b5406a75cbcb5037
leakp <lea.kampermann@gmail.com>
2016-03-14 17:02:25 +0100

    changes in conditioning barplot

--- Perception_and_Ratings.m
+++ Perception_and_Ratings.m
@@ -5,52 +5,63 @@ p = Project;
 mask = p.getMask('RATE');
 subs = intersect(find(mask),Project.subjects_1500);
 g = Group(subs);
-ratings = g.getRatings(2:4);
+[ratings sds]= g.getRatings(2:4);
 %%
 clf
 c = 0;
 for n = 2:4
     c = c+1;
     subpl(c) =  subplot(3,3,c);
-    b = bar(1:8,mean(ratings(:,:,n)));
+    b = bar(-135:45:180,mean(ratings(:,:,n)));
     hold on;
-    e = errorbar(1:8,mean(ratings(:,:,n)),std(ratings(:,:,n))./sqrt(size(ratings,1)),'k.');
-    set(gca,'XTick',1:8,'XTickLabel',{'' '' '' 'CS+' '' '' '' 'CS-'})
+    e = errorbar(-135:45:180,mean(ratings(:,:,n)),std(ratings(:,:,n))./sqrt(size(ratings,1)),'k.');
+    set(gca,'XTick',-135:45:180,'XTickLabel',{'' '' '' 'CS+' '' '' '' 'CS-'})
     SetFearGenBarColors(b)
     set(e,'LineWidth',2,'Color','k')
     ylim([0 10])
-    xlim([0 9])
+    xlim([-180 225])
     axis square
     box off
 end
 subplot(3,3,1);ylabel('Rating of p(shock)')
+%add Groupfit line
+
+x = -150:0.1:195;
+subplot(3,3,1);
+plot(x,mean(mean(ratings(:,:,2))),'k-','LineWidth',1.5)
+subplot(3,3,2);
+plot(x,VonMises(deg2rad(x),5.4473,1.3072,deg2rad(-8.0356),1.7061),'k-','LineWidth',1.5)
+subplot(3,3,3);
+plot(x,VonMises(deg2rad(x),4.6438,2.2963,deg2rad(-1.4574),1.9557),'k-','LineWidth',1.5)
+
 %%scr 
-load('C:\Users\user\Documents\Experiments\FearCloud_Eyelab\data\midlevel\scr_600BCT.mat','subjects','scr_bars')
-ylims = [.3 1.1];%600   %1500: 0.7
-Yticks = [0.3 0.6 0.9];%600, %1500: [0 0.3 0.6]
+load('C:\Users\user\Documents\Experiments\FearCloud_Eyelab\data\midlevel\scr_1500BCT.mat','subjects','scr_bars')
+ylims = [0 0.7];%600 0.3 1.1  %1500: 0.7
+Yticks = [0 0.3 0.6];%600 [0.3 0.6 0.9], %1500: [0 0.3 0.6]
 
 % plot
 for n = [1 3]
 subpl(3+n) = subplot(3,3,3+n);
-b = bar(1:8,mean(scr_bars(:,1:8,n)));
+b = bar(-135:45:180,mean(scr_bars(:,1:8,n)));
 hold on;
 ylim(ylims)
-xlim([0 9])
-e = errorbar(1:8,mean(scr_bars(:,1:8,n)),std(scr_bars(:,1:8,n))./sqrt(size(scr_bars,1)),'k.');
-set(gca,'XTick',1:8,'XTickLabel',{'' '' '' 'CS+' '' '' '' 'CS-'},'YTick',Yticks)
+xlim([-180 225])
+e = errorbar(-135:45:180,mean(scr_bars(:,1:8,n)),std(scr_bars(:,1:8,n))./sqrt(size(scr_bars,1)),'k.');
+set(gca,'XTick',-135:45:180,'XTickLabel',{'' '' '' 'CS+' '' '' '' 'CS-'},'YTick',Yticks)
 SetFearGenBarColors(b)
 set(e,'LineWidth',2,'Color','k')
 axis square
 box off
 end
+
 subplot(3,3,4);ylabel('SCR [muS]')
 %cond special treatment
 subpl(5) = subplot(3,3,5);
+
 b(1) = bar(4,mean(scr_bars(:,4,2)));
 hold on;
 e(1) = errorbar(4,mean(scr_bars(:,4,2)),std(scr_bars(:,4,2))./sqrt(size(scr_bars,1)),'k.');
 ylim(ylims)
-xlim([0 9])
 set(gca,'XTick',1:8,'XTickLabel',{'' '' '' 'CS+' '' '' '' 'CS-'},'YTick',Yticks);
 b(2) =  bar(8,mean(scr_bars(:,8,2)));
 e(2) = errorbar(8,mean(scr_bars(:,8,2)),std(scr_bars(:,8,2))./sqrt(size(scr_bars,1)));
@@ -59,22 +70,66 @@ set(b(2),'FaceColor','c','EdgeColor','w');
 set(e,'LineWidth',2,'Color','k');
 axis square
 box off
+xlim([0 9])
 try
     for n = 1:size(scr_bars,3)
         subplot(3,3,3+n)
-        line([0 9],repmat(mean(scr_bars(:,9,n)),[2 1]),'Color','k','LineWidth',1.3,'LineStyle',':')
+        line([-180 225],repmat(mean(scr_bars(:,9,n)),[2 1]),'Color','k','LineWidth',1.3,'LineStyle',':')
     end
 end
 
-t = supertitle('Conditioning in 600 ms');set(t,'FontSize',14)
+% add von mises groupfit curve to SCR bars @testphase
+subplot(3,3,6);
+plot(x,VonMises(deg2rad(x),0.1678,2.2181,deg2rad(-5.0893),0.2204),'k-','LineWidth',1.5)
+subplot(3,3,4)
+plot(x,mean(mean(scr_bars(:,1:8,1))),'k-','LineWidth',1.5)
+% single subject example
+% sub = 7;
+% g2 = Group(sub); g2.ModelRatings(2,8);g2.ModelRatings(3,8);g2.ModelRatings(4,8);
+% [rate sds] = g2.getRatings(2:4);
+
+for sub = 1:length(g.ids)
+    for n = 7:9
+        subplot(3,3,n)
+        b=bar(-135:45:180,rate(:,:,n-5));
+        SetFearGenBarColors(b);
+        hold on;
+        errorbar(-135:45:180,rate(:,:,n-5),sds(:,:,n-5)./sqrt(2),'k.','LineWidth',2)
+        set(gca,'XTick',-135:45:180,'XTickLabel',{'' '' '' 'CS+' '' '' '' 'CS-'},'YTick',[0,5,10])
+        params = g2.tunings.rate{n-5}.singlesubject{1}.Est(1:4);
+        plot(x,VonMises(deg2rad(x),params(1),params(2),deg2rad(params(3)),params(4)),'k-','LineWidth',1.5)
+        ylim([0 11])
+        axis square
+        box off
+    end
+end
+
+%% SEARCH
+% single subject example
+clf
+for sub = 1:length(g.ids)
+    close all
+    for n = 7:9
+        subplot(3,3,n)
+        b=bar(-135:45:180,ratings(sub,:,n-5));
+        SetFearGenBarColors(b);
+        hold on;
+        errorbar(-135:45:180,ratings(sub,:,n-5),sds(sub,:,n-5)./sqrt(2),'k.','LineWidth',2)
+        set(gca,'XTick',-135:45:180,'XTickLabel',{'' '' '' 'CS+' '' '' '' 'CS-'},'YTick',[0,5,10])
+        ylim([0 10])
+        title(num2str(sub))
+    end
+    pause
+end
 %% add generalization in fixations
+p =Project;
 mask = p.getMask('ET_feargen');
-subjects = intersect(find(mask),Project.subjects_600);
+subjects = intersect(find(mask),Project.subjects_1500);
 fix = Fixmat(subjects,2:4);
 counts0 = fix.histogram;close;
 %cond: 11=null, 10=oddball, 9=ucs
 for n = 1:3
-counts(:,:,n) = nanzscore(counts0(:,:,n)')';
+    counts(:,:,n) = nanzscore(counts0(:,1:8,n)')';
 end
 c = 0;
 for n = 1:3
@@ -271,8 +326,10 @@ set(e,'LineWidth',2)
 
 % plot bars and Gaussians in Feargen Colors
 %
-g.ModelRatings(3,8)
-g.ModelRatings(4,8)
+% g.ModelRatings(3,8)
+% g.ModelRatings(4,8)
+g.tunings.rate{3} = Tuning(g.Ratings(3));
+g.tunings.rate{4} = Tuning(g.Ratings(4));
 g.tunings.rate{3}.GroupFit(8)
 g.tunings.rate{4}.GroupFit(8)
 g.PlotRatingResults
