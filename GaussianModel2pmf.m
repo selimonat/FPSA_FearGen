b0b010c1563132e2b802dc42490d3d4b7b4bd00d
onatselim <onatselim@gmail.com>
2016-04-29 18:38:44 +0200

    script to model subject's perceptual performance with Gaussians (sigma) and threshold values (lamda) and their mapping onto different Weibull pmf parameters

---
+++ GaussianModel2pmf.m
@@ -0,0 +1,37 @@
+addpath('/Users/onat/Documents/Code/Matlab/palamedes1_8_0/Palamedes/')
+PF                  = @PAL_Weibull;
+options             = PAL_minimize('options');
+options.MaxIter     = 10.^6;
+options.MaxFunEvals = 10.^6;
+options.Display     = 'On';
+options.TolX        = 10.^-4;
+options.TolFun      = 10.^-4;
+%%
+sigmas   = linspace(10,90,25);%subject's precision of face representation in degrees
+lambdas  = linspace(0,90,500);%subject's threshold
+stims    = linspace(0,180,200);%location of stimuli
+data     = NaN(length(stims),length(sigmas),length(lambdas));
+params   = NaN(4,length(sigmas),length(lambdas));
+lc       = 0;
+%%
+for lambda = lambdas(:)'
+    lambda
+    lc = lc + 1;sc = 0;
+    for sig = sigmas(:)'
+        sc = sc+1;cc = 0;        
+        data(:,sc,lc) = 1-normcdf(lambda,stims,sig);
+        %
+        searchGrid.alpha  = 15;
+        searchGrid.beta   = 2;%10.^pmf.beta(chain);
+        searchGrid.gamma  = data(1,sc,lc);%pmf.gamma(chain);
+        searchGrid.lambda = 1-data(end,sc,lc);%pmf.lambda(chain);
+        params0 = [ searchGrid.alpha  searchGrid.beta searchGrid.gamma searchGrid.lambda];
+        %        
+        funny                                  = @(params) sum( (data(:,sc,lc)' - PF(params,stims)).^2);
+        options                                = optimset('Display','off','maxfunevals',10000,'tolX',10^-12,'tolfun',10^-12,'MaxIter',10000,'Algorithm','interior-point');
+        [o.params1, o.Likelihood, o.ExitFlag]  = fmincon(funny, params0, [],[],[],[],[-Inf -Inf 0 0],[Inf Inf 1 1],[],options);
+        plot(data(:,sc,lc));hold on;
+        plot(PF(o.params1,stims),'ro');hold off;drawnow;
+        params(:,sc,lc) = o.params1;
+    end
+end
