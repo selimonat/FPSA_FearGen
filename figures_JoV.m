cbba4f72382e5f325d4b741d42b3e45d038e9aa3
leakp <lea.kampermann@gmail.com>
2016-06-15 10:44:59 +0200

    added figure 2x5 for behavioral results

--- figures_JoV.m
+++ figures_JoV.m
@@ -31,38 +31,165 @@ fix.getmaps(v{:});
 fix.plot
 %%
 %%%%%%%%%%%%%
-% behavioral results
+% behavioral results 
 %%%%%%%%%%%%%
-%histogram of alpha and kappa for both durations
+%% big figure for behavioral results
+load('C:\Users\user\Documents\Experiments\FearCloud_Eyelab\data\midlevel\ratings_600.mat','ratings');
+sp = [2 5];
+clf
+c = 6;
+for n = 2:4
+    c = c+1;
+    subpl(c) =  subplot(sp(1),sp(2),c);
+    b = bar(-135:45:180,mean(ratings(:,:,n)));
+    hold on;
+    e = errorbar(-135:45:180,mean(ratings(:,:,n)),std(ratings(:,:,n))./sqrt(size(ratings,1)),'k.');
+    set(gca,'XTick',-135:45:180,'XTickLabel',{'' '' '' 'CS+' '' '' '' 'CS-'},'YTick',[0 5 10])
+    SetFearGenBarColors(b)
+    set(e,'LineWidth',2,'Color','k')
+    ylim([0 10])
+    xlim([-180 225])
+    axis square
+    box off
+end
+subplot(sp(1),sp(2),7);ylabel('Rating of p(shock)')
+%add Groupfit line
+params = [5.1801 0.7288 deg2rad(0.8714) 1.8312; 4.4396 1.1171 deg2rad(-0.0598) 1.9506]; %600ms cond;test
+% params = [5.4473,1.3072,deg2rad(-8.0356),1.7061;4.6438,2.2963,deg2rad(-1.4574),1.9557] %1500 cond;test
+x = -150:0.1:195;
+subplot(sp(1),sp(2),7);
+plot(x,mean(mean(ratings(:,:,2))),'k-','LineWidth',3.5)
+subplot(sp(1),sp(2),8);
+plot(x,VonMises(deg2rad(x),params(1,1),params(1,2),params(1,3),params(1,4)),'k-','LineWidth',1.5)
+line([0 180],[9 9],'Color','k','LineWidth',1.3)
+text(45,9,'***','FontSize',20)
+subplot(sp(1),sp(2),9);
+plot(x,VonMises(deg2rad(x),params(2,1),params(2,2),params(2,3),params(2,4)),'k-','LineWidth',1.5)
+line([0 180],[9 9],'Color','k','LineWidth',1.3)
+text(45,9,'***','FontSize',20)
+
+%%scr 
+load('C:\Users\user\Documents\Experiments\FearCloud_Eyelab\data\midlevel\scr_600BCT.mat','scr_bars','subjects')
+ylims = [0.3 .9];%600 0.3 1.1  %1500: 0.7
+Yticks = [0.3 0.6 0.9];%600 [0.3 0.6 0.9], %1500: [0 0.3 0.6]
+
+% plot
+for n = [1 3]
+subplot(sp(1),sp(2),n+1);
+b = bar(-135:45:180,mean(scr_bars(:,1:8,n)));
+hold on;
+ylim(ylims)
+xlim([-180 225])
+e = errorbar(-135:45:180,mean(scr_bars(:,1:8,n)),std(scr_bars(:,1:8,n))./sqrt(size(scr_bars,1)),'k.');
+set(gca,'XTick',-135:45:180,'XTickLabel',{'' '' '' 'CS+' '' '' '' 'CS-'},'YTick',Yticks)
+SetFearGenBarColors(b)
+set(e,'LineWidth',2,'Color','k')
+axis square
+box off
+end
+subplot(sp(1),sp(2),2);ylabel('SCR [muS]')
+title('Free Viewing')
+subplot(sp(1),sp(2),4)
+title('Generalization')
+%cond special treatment
+subplot(sp(1),sp(2),3);
+b(1) = bar(4,mean(scr_bars(:,4,2)));
+hold on;
+e(1) = errorbar(4,mean(scr_bars(:,4,2)),std(scr_bars(:,4,2))./sqrt(size(scr_bars,1)),'k.');
+ylim(ylims)
+set(gca,'XTick',1:8,'XTickLabel',{'' '' '' 'CS+' '' '' '' 'CS-'},'YTick',Yticks);
+b(2) =  bar(8,mean(scr_bars(:,8,2)));
+e(2) = errorbar(8,mean(scr_bars(:,8,2)),std(scr_bars(:,8,2))./sqrt(size(scr_bars,1)));
+set(b(1),'FaceColor',[1 0 0],'EdgeColor','w');
+set(b(2),'FaceColor','c','EdgeColor','w');
+set(e,'LineWidth',2,'Color','k');
+axis square
+box off
+xlim([0 9])
+line([4 8],[.85 0.85],'Color','k','LineWidth',1.3)
+text(5.7,.85,'*','FontSize',20)
+title('Conditioning')
+
+try
+    for n = 1:size(scr_bars,3)
+        subplot(sp(1),sp(2),1+n)
+        line([-180 225],repmat(mean(scr_bars(:,9,n)),[2 1]),'Color','k','LineWidth',1.3,'LineStyle',':')
+    end
+end
+% 
+% % add von mises groupfit curve to SCR bars @testphase
+subplot(sp(1),sp(2),2);
+plot(x,mean(mean(scr_bars(:,1:8,1))),'k-','LineWidth',3.5)
+subplot(sp(1),sp(2),4);
+plot(x,mean(mean(scr_bars(:,1:8,3))),'k-','LineWidth',3.5)
+
+% pmf at 1 and 5
+load('C:\Users\user\Documents\Experiments\FearCloud_Eyelab\data\midlevel\params_pmf_600.mat','params')
+pmf = mean(params,3);
+pmf(:,2) = repmat(mean(params(:,2)),[4 1]);
+x = linspace(0,180,1000);
+subplot(sp(1),sp(2),1);
+plot(x,PAL_Weibull(pmf(2,:),x),'c','LineWidth',3)%csn before
+hold on;
+plot(x,PAL_Weibull(pmf(1,:),x),'r','LineWidth',3)%csp before
+set(gca,'YTick',0:.25:1);
+axis square
+box off
+ylim([0 1]);
+xlim([0 120]);
+subplot(sp(1),sp(2),5);
+plot(x,PAL_Weibull(pmf(4,:),x),'c','LineWidth',3)%csn after
+hold on;
+plot(x,PAL_Weibull(pmf(3,:),x),'r','LineWidth',3)%csp after
+set(gca,'YTick',0:.25:1);
+axis square
+box off
+ylim([0 1]);
+xlim([0 120]);
+legend('CS-','CS+','location','southeast')
+legend boxoff
+hold off
+%% histogram of alpha and kappa for both durations
 clf
 load('C:\Users\user\Documents\Experiments\FearCloud_Eyelab\data\midlevel\rate2pmf_N54.mat')
 ind = subs15;
 sp = [2 4];
+crit = 22.5; % for later noshifter vs shifter improvement
+
 subplot(sp(1),sp(2),1)%FWHM
-y = ksdensity(mat(ind,18),'bandwidth',20);
-plot(y,'b','LineWidth',2);hold on;
-y = ksdensity(mat(ind,19),'bandwidth',20);
-plot(y,'k','LineWidth',2)
+[y,xi] = ksdensity(mat(ind,18),'bandwidth',10);
+plot(xi,y,'b','LineWidth',2);hold on;
+[y,xi] = ksdensity(mat(ind,19),'bandwidth',10);
+plot(xi,y,'k','LineWidth',2)
 axis square;box off
+hold off
+% xlim([min(abs(mat(ind,16))) max(abs(mat(:,16)))])\
+xlim([0 180])
 % set(gca,'YTick',yticks,'XTick',xticks,'XTickLabel',{'wide' 'sharp'})
 ylabel('density')
 title('FWHM')
+text(10,7.8e-3,'A','FontSize',20);
 subplot(sp(1),sp(2),2)%MU
-y = ksdensity(abs(mat(ind,15)),'bandwidth',7);
-plot(y,'b','LineWidth',2);hold on;
-y = ksdensity(abs(mat(ind,16)),'bandwidth',7);
-plot(y,'k','LineWidth',2)
+[y,xi] = ksdensity(abs(mat(ind,15)),'bandwidth',7);
+plot(xi,y,'b','LineWidth',2);hold on;
+[y,xi] = ksdensity(abs(mat(ind,16)),'bandwidth',7);
+plot(xi,y,'k','LineWidth',2)
 legend('Conditioning','Generalization')
 legend boxoff
 axis square;box off
+xlim([min(abs(mat(ind,16))) max(abs(mat(:,16)))])
 % set(gca,'YTick',yticks,'XTick',xticks,'XTickLabel',{'wide' 'sharp'})
 ylabel('density')
 title('MU')
+line([crit crit],ylim,'Color','k')
 hold off
 %
 load('C:\Users\user\Documents\Experiments\FearCloud_Eyelab\data\midlevel\rate_and_pmf_N48.mat')
-load('C:\Users\user\Documents\Experiments\FearCloud_Eyelab\data\midlevel\max4.mat')
+% load('C:\Users\user\Documents\Experiments\FearCloud_Eyelab\data\midlevel\max4.mat')
+ind = logical(abs(mat6(:,16))<= crit);
 subplot(sp(1),sp(2),5)%alpha results
+text(.5,78,'B','FontSize',20);
+ylabel('threshold alpha (deg)')
 b = bar([1 2 4 5],mean(mat6(:,1:4)));
 SetFearGenBarColors(b,[1 0 0;.8 0 0;0 1 1;0 .8 .8]')
 line([1 2],[72 72],'Color','k','LineWidth',2)
@@ -75,53 +202,50 @@ set(e,'LineWidth',2)
 axis square;box off;
 xlim([0 6])
 subplot(sp(1),sp(2),6)%csp improvement shifters vs no shifters
-b = bar([1 4],[mean(mat6(max4,11)) mean(mat6(~max4,11))]);
-set(b,'BarWidth',1)
+b = bar(1:2,[mean(mat6(ind,11)) mean(mat6(~ind,11))]);
+set(b,'BarWidth',.5,'FaceColor','k')
 hold on;box off
-e = errorbar([1 4],[mean(mat6(max4,11)) mean(mat6(~max4,11))],[std(mat6(max4,11))./sqrt(sum(max4)) std(mat6(~max4,11))./sqrt(sum(~max4))],'k.','LineWidth',1.5);
+e = errorbar(1:2,[mean(mat6(ind,11)) mean(mat6(~ind,11))],[std(mat6(ind,11))./sqrt(sum(ind)) std(mat6(~ind,11))./sqrt(sum(~ind))],'k.','LineWidth',1.5);
+e = errorbar(1,mean(mat6(ind,11)),std(mat6(ind,11))./sqrt(sum(ind)),0,'w.','LineWidth',1.5);
 axis square
-xlim([-2 7])
-set(gca,'XTick',[1 4],'XTicklabel',{'CS+ centered' 'shifted'})
+xlim([0 3])
+set(gca,'XTick',[1 2],'XTicklabel',{'CS+ centered' 'shifted'})
+ylim([-5 20])
+ylabel('corrected improvement (deg)')
 hold off;
 %% graph alpha x discrimination (binning)
 load('C:\Users\user\Documents\Experiments\FearCloud_Eyelab\data\midlevel\rate_and_pmf_N48.mat');
 linewidth = 3;
-x_pos = [1.05:1:3.05;.95:1:2.95;1.05:1:3.05;.95:1:2.95];
+subplot(sp(1),sp(2),[3 4 7 8])
+text(.5,5.5,'C','FontSize',20);
 % bin perception, plot kappa
-clf
-subplot(1,2,1)
-col = [1 3];
-p = prctile(mean(mat(:,col),2),linspace(0,100,4));
-
 % 600ms
 mat = mat6;
+col = [1 3];
 p = prctile(mean(mat(:,col),2),linspace(0,100,4));
-
 outcome_m = [];outcome_s=[];
-for n = 1:length(p)-1    
-    i = (mean(mat(:,col),2) > p(n)) & (mean(mat(:,col),2) <= p(n+1));
+for n = 1:length(p)-1   
+    if n ==1
+        i = (mean(mat(:,col),2) >= p(n)) & (mean(mat(:,col),2) <= p(n+1));
+    else
+        i = (mean(mat(:,col),2) >= p(n)) & (mean(mat(:,col),2) <= p(n+1));
+    end
     sum(i)
+    mean(mean(mat(i,[1 3]),2))   
     binalpha(4,n) = mean(mean(mat(i,[1 3]),2));
-    outcome_m(n) = mean(mean(mat(i,[13]),2));
-    outcome_s(n) = std(mean(mat(i,[13]),2))./sqrt(sum(i)); 
+    outcome_m(n) = mean(mean(mat(i,[12]),2));
+    outcome_s(n) = std(mean(mat(i,[12]),2))./sqrt(sum(i)); 
 end
-errorbar(binalpha(4,:),outcome_m,outcome_s,'k','LineWidth',linewidth)
-legend('1500 ms','600 ms')
-legend boxoff
-for n=1:2
-subplot(1,2,n)
-% set(gca,'XTick',1:3,'XTicklabel',{'good','medium','bad'},'YTick',2:2:10);
-ylim([0 10])
-ylabel('FearGen specificity (kappa)')
-xlabel('Discrimination performance')
-box off
+bar(1:3,outcome_m,'FaceColor','r');hold on;
+bar(1:3,[2.59 .995 .147],'FaceColor',[.3 .3 .3])
+errorbar(1:3,outcome_m,outcome_s,'k.','LineWidth',linewidth)
+set(gca,'XTickLabel',{'good','moderate','weak'})
+xlabel('discrimination performance')
+ylabel('fear specificity (kappa)')
 axis square
-end
-subplot(1,2,1);title('Conditioning')
-subplot(1,2,2);title('Test phase')
-
-
-
+box off
+xlim([0 4])
+hold off
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Fig 2 - beneficial locations
