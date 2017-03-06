f417b2ca83f7e2e771a6d4374b968d8aca544bba
leakp <lea.kampermann@gmail.com>
2017-03-06 11:13:27 +0100

    first import of SVM classification

--- FearCloud_RSA.m
+++ FearCloud_RSA.m
@@ -1108,6 +1108,175 @@ elseif strcmp(varargin{1},'anova')
     side     = [ones(tsubject,1);ones(tsubject,1)*2;ones(tsubject,1);ones(tsubject,1)*2];
     phase    = [ones(tsubject,1);ones(tsubject,1);ones(tsubject,1)*2;ones(tsubject,1)*2];
     anovan(y,{side(:) phase(:)},'model','full')
+elseif strcmp(varargin{1},'SVM')
+    %This script trains a linear SVM training CS+ vs. CS- for phases 2 and 4.
+    %It collects the data and computes the eigenvalues on the
+    %fly for chosen(or a range of parameters) (kernel_fwhm, number of
+    %eigenvalues). As the number of trials are lower in the
+    %baseline, all the test-training sessions should use the same number of
+    %trials. For example to keep the comparisons comparable, in baseline 11
+    %trials in the baseline, with .5 hold-out, one needs to sample the same
+    %number of trials from the testphase before training.
+    %the option random = 1 randomizes labels to determine the chance classification
+    %performance level.
+    random = 0;
+    
+    tbootstrap       = 1000; %number of bootstraps
+    phase            = [2 4 4 4];%baseline = 2, test = 4
+    holdout_ratio    = .5; %holdout_ratio for training vs. test set
+    teig             = 100; %up to how many eigenvalues should be included for tuning SVM?
+    crit             = 'max';%choose 'max' classification or 'var' 90% variance explained.
+    cutoffcrit       = .90;
+    R                = [];%result storage for classification performance
+    HP               = [];%result storage for single subject hyperplanes.
+    AVEHP            = [];%result storate for average hyperplane
+    
+    eigval           = [];
+    trialselect      = {1:120 1:120 121:240 241:360};
+    
+    
+    subjects = FearCloud_RSA('get_subjects');
+    o = 0;
+    for opt = 1:4 % phase 2, phase 4.1 phase 4.2 phase 4.3
+        o = o+1;
+        fix             = Fixmat(subjects,phase(opt));%get the data
+        fix.unitize     = 1;%unitize fixmaps or not (sum(fixmap(:))=0 or not).
+        %% get number of trials per condition and subject: Sanity check...
+        M               = [];%this will contain number of trials per subject and per condition. some few people have 10 trials (not 11) but that is ok. This is how I formed the subject_exlusion variable.
+        sub_c           = 0;
+        for ns = subjects(:)'
+            sub_c = sub_c + 1;
+            nc = 0;
+            for cond = unique(fix.deltacsp)
+                nc          = nc +1;
+                i           = ismember(fix.phase,phase(opt)).*ismember(fix.subject,ns).*ismember(fix.deltacsp,cond);%this is on fixation logic, not trials.
+                i           = logical(i);
+                M(sub_c,nc,o) = length(unique(fix.trialid(i)));
+            end
+        end
+        %% get all the single trials in a huge matrix D together with labels.
+        global_counter = 0;
+        clear D;%the giant data matrix
+        clear labels;%and associated labels.
+        for ns = subjects(:)'
+            for deltacsp = -135:45:180;
+                i              = ismember(fix.trialid,trialselect{opt}).*(fix.subject == ns).*(fix.deltacsp == deltacsp);
+                trials         = unique(fix.trialid(i == 1));
+                trial_counter  = 0;
+                for trialid = trials
+                    trial_counter       = trial_counter + 1;
+                    global_counter      = global_counter +1;
+                    c                   = global_counter;
+                    v                   = {'subject' ns 'deltacsp' deltacsp 'trialid' trialid};
+                    fix.getmaps(v);
+                    D(:,c)              = Vectorize(imresize(fix.maps,.1));
+                    labels.sub(c)       = ns;
+                    labels.phase(c)     = phase(opt);
+                    labels.trial(c)     = trial_counter;%some people have less trials check it out with plot(labels.trial)
+                    labels.cond(c)      = deltacsp;
+                end
+            end
+        end
+        %% DATA2LOAD get the eigen decomposition: D is transformed to TRIALLOAD
+        fprintf('starting covariance computation\n')
+        covmat    = cov(D');
+        fprintf('done\n')
+        fprintf('starting eigenvector computation\n')
+        [e dv]    = eig(covmat);
+        fprintf('done\n')
+        dv        = sort(diag(dv),'descend');
+        eigval(:,opt) = dv;
+        %     figure(100);
+        %     plot(cumsum(dv)./sum(dv),'o-');xlim([0 200]);drawnow
+        eigen     = fliplr(e);
+        %collect loadings of every trial
+        trialload = D'*eigen(:,1:teig)*diag(dv(1:teig))^-.5;%dewhitened
+        %% LIBSVM business
+        neigs = [7 12 8 10];
+        cev = 0;
+        for neig    = neigs(opt);%1:teig%test different numbers of eigenvectors
+            cev = cev+1;
+            sub_counter = 0;
+            result      = [];
+            w           = [];
+            for sub = unique(labels.sub)%go subject by subject
+                fprintf('Opt:%d-Eig:%d-Sub:%d\n',opt,neig,sub);
+                if random == 1
+                    warning('Randomizing labels as wanted. \n')
+                end
+                sub_counter = sub_counter + 1;
+                ind_all     = ismember(labels.sub,sub);%this subject, this phase.
+                %
+                for n = 1:tbootstrap%
+                    Ycond   = double(labels.cond(ind_all))';%labels of the fixation maps for this subject in this phase.
+                    X       = trialload(ind_all,1:neig);%fixation maps of this subject in this phase.
+                    % now normal Holdout for every phase (which should all have the
+                    % same number of trials now)
+                    P       = cvpartition(Ycond,'Holdout',holdout_ratio); % divide training and test datasets respecting conditions
+                    i       = logical(P.training.*ismember(Ycond,[0 180]));%train using only the CS+ and CS? conditions.
+                    if random ==1
+                        model   = svmtrain(Shuffle(Ycond(i)), X(i,1:neig), '-t 0 -c 1 -q'); %t 0: linear, -c 1: criterion, -q: quiet
+                    else
+                        model   = svmtrain(Ycond(i), X(i,1:neig), '-t 0 -c 1 -q'); %t 0: linear, -c 1: criterion, -q: quiet
+                    end
+                    % get the hyperplane
+                    try
+                        w(:,sub_counter,n)          = model.SVs'*model.sv_coef;
+                    catch
+                        keyboard%sanity check: stop if something is wrong
+                    end
+                    %%
+                    cc=0;
+                    for cond = unique(Ycond)'
+                        cc                          = cc+1;
+                        i                           = logical(P.test.*ismember(Ycond,cond));%find all indices that were not used for training belonging to COND.
+                        [~, dummy]                  = evalc('svmpredict(Ycond(i), X(i,:), model);');%doing it like this supresses outputs.
+                        dummy                       = dummy == 0;%binarize: 1=CS+, 0=Not CS+
+                        result(cc,n,sub_counter)    = sum(dummy)/length(dummy);%get the percentage of CS+ responses for each CONDITION,BOOTSTR,SUBJECT
+                    end
+                end
+            end
+            %once the data is there compute relevant output metrics:
+            R(:,cev,opt)      = mean(mean(result,2),3);%average across bootstaps the classification results
+            AVEHP(:,:,cev,opt) = reshape(mean(eigen(:,1:neig)*mean(w,3),2),[50 50 1]);%average hyperplane across subjects
+            HP(:,:,:,cev,opt) = reshape(eigen(:,1:neig)*mean(w,3),[50 50 size(eigen(:,1:neig)*mean(w,3),2)]); %single hyperplanes
+        end
+        %     figure(1000);imagesc(R(:,:,opt));
+        %     figure(1001);plot(R(4,:,opt)-R(end,:,opt),'o-');
+        %     drawnow
+    end
+    %% plot
+    
+    clf
+    perf = squeeze(R(4,:,:)-R(end,:,:));
+    [~,neig] =max(perf);
+    %the numbers we extracted:
+    neigs =  [7 12 8 10];
+    
+    for opt = 1:4
+        load(['C:\Users\Lea\Documents\Experiments\project_bdnf\data\midlevel\svm_analysis\findingparams\' sprintf('SVM_NEV%d_FWHM30_r0_opt%d.mat',neigs(opt),opt)])
+        M = mean(mean(result,2),3);
+        SE = std(mean(result,2),[],3)./sqrt(size(result,3));
+        figure(100);
+        subplot(1,5,opt);
+        b = bar(M);SetFearGenBarColors(b);
+        hold on;
+        errorbar(M,SE,'k.','LineWidth',2)
+        ylim([.3 .7])
+        set(gca,'YTick',.3:.1:.7,'XTick',[4 8],'XTickLabel',{'CS+' 'CS-'});
+        box off
+        axis square
+        ylabel('Classified as CS+')
+    end
+    labels = {'baseline' 'test_1' 'test_2' 'test_3' 'test_{all}'};
+    for n = 1:4
+        subplot(1,5,n)
+        ylim([.25 .75])
+        set(gca,'YTick',[.3 .5 .7])
+        xlim([0 9])
+        title(labels{n})
+    end
+
 elseif strcmp(varargin{1},'figure_01');
     %% this id the cartoon figure where hypotheses are presented;
     set(gcf,'position',[2588         146        1212         659]);
